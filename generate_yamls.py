56b997da989325b207e638b8f6ed0cb89dad31d1
Deblob12 <jeffrey_chiu@college.harvard.edu>
2023-04-28 17:45:41 -0400

    Yaml Scripts

---
+++ generate_yamls.py
@@ -0,0 +1,14 @@
+import yaml
+with open('example.yaml', 'r') as file:
+    cpu_yaml_template = yaml.safe_load(file)
+    
+cpus = [2,4,8]
+steps = [1e9, 5e9, 1e10, 5e10, 1e11]
+
+for cpu in cpus:
+    for step in steps:
+        cpu_yaml_template['resources']['cpus'] = cpu
+        cpu_yaml_template['run'] = f'python cpu_task.py {int(step)}'
+        
+        with open(f'./cpu_yamls/{cpu}cpus_{int(step)}steps.yaml', 'w') as file:
+            yaml.dump(cpu_yaml_template, file)
