da8686f81870a89c046a8017f575ff2bd4af2aab
Deblob12 <jeffrey_chiu@college.harvard.edu>
2023-04-29 16:33:49 -0400

    CPU

--- generate_yamls.py
+++ generate_yamls.py
@@ -8,7 +8,7 @@ steps = [1e9, 5e9, 1e10, 5e10, 1e11]
 for cpu in cpus:
     for step in steps:
         cpu_yaml_template['resources']['cpus'] = cpu
-        cpu_yaml_template['run'] = f'python cpu_task.py {int(step)}'
+        cpu_yaml_template['run'] = f"python cpu_task.py {int(cpu)} {int(step)}"
         
         with open(f'./cpu_yamls/{cpu}cpus_{int(step)}steps.yaml', 'w') as file:
-            yaml.dump(cpu_yaml_template, file)
+            yaml.dump(cpu_yaml_template, file, sort_keys=False)
