11b94e68143773d3961b2c834f1aa6f7af68366e
Deblob12 <jeffrey_chiu@college.harvard.edu>
2023-05-01 14:51:15 -0400

    GPUs

---
+++ scripts/gpu_task.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process
+from sys import argv
+import math
+
+
+def task(iterations):
+    device = torch.device("cuda")
+    # Create 512x512 identity matrices on the GPU
+    matrix_A = torch.eye(512, dtype=torch.float32, device=device)
+    matrix_B = torch.eye(512, dtype=torch.float32, device=device)
+
+
+    # Perform the matrix multiplications
+    for _ in range(iterations):
+        result = torch.matmul(matrix_A, matrix_B)
+
+
+if __name__ == '__main__':
+    total_iterations = int(argv[1])
+    task(total_iterations)
